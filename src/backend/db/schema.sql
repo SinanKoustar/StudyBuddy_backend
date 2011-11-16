112103195ce2eae7e35330b7d32527c3ff915607
SinanKoustar <sinan46@web.de>
2011-11-16 19:59:40 +0100

    Database has been changed. A new table has been created and the names of the columns has been changed

--- src/backend/db/schema.sql
+++ src/backend/db/schema.sql
@@ -1,50 +1,134 @@
 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+
 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL';
 
+
+
 CREATE SCHEMA IF NOT EXISTS `mydb` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci ;
+
 USE `mydb` ;
 
+
+
 -- -----------------------------------------------------
+
+-- Table `mydb`.`Category`
+
+-- -----------------------------------------------------
+
+CREATE  TABLE IF NOT EXISTS `mydb`.`Category` (
+
+  `idCategory` INT NOT NULL AUTO_INCREMENT ,
+
+  `Description` VARCHAR(15) NOT NULL ,
+
+  PRIMARY KEY (`idCategory`) ,
+
+  UNIQUE INDEX `Discription_UNIQUE` (`Description` ASC) )
+
+ENGINE = InnoDB;
+
+
+
+
+
+-- -----------------------------------------------------
+
 -- Table `mydb`.`Notes`
+
 -- -----------------------------------------------------
+
 CREATE  TABLE IF NOT EXISTS `mydb`.`Notes` (
+
   `idNotes` INT NOT NULL AUTO_INCREMENT ,
-  `Betreff` VARCHAR(45) NOT NULL ,
-  `Nachricht` VARCHAR(161) NOT NULL ,
-  `Eintragsdatum` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ,
+
+  `Subject` VARCHAR(45) NOT NULL ,
+
+  `Message` VARCHAR(161) NOT NULL ,
+
+  `Creation_Date` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ,
+
+  `Status` TINYINT(1)  NOT NULL DEFAULT TRUE ,
+
   `UserID` INT NOT NULL ,
+
+  `CategoryID` INT NOT NULL ,
+
   PRIMARY KEY (`idNotes`) ,
-  UNIQUE INDEX `idNotes_UNIQUE` (`idNotes` ASC) )
+
+  INDEX `idCategory` (`CategoryID` ASC) ,
+
+  CONSTRAINT `idCategory`
+
+    FOREIGN KEY (`CategoryID` )
+
+    REFERENCES `mydb`.`Category` (`idCategory` )
+
+    ON DELETE NO ACTION
+
+    ON UPDATE NO ACTION)
+
 ENGINE = InnoDB;
 
 
+
+
+
 -- -----------------------------------------------------
+
 -- Table `mydb`.`User`
+
 -- -----------------------------------------------------
+
 CREATE  TABLE IF NOT EXISTS `mydb`.`User` (
+
   `idUser` INT NOT NULL AUTO_INCREMENT ,
-  `Benutzername` VARCHAR(20) NOT NULL ,
-  PRIMARY KEY (`idUser`) ,
-  UNIQUE INDEX `Benutzername_UNIQUE` (`Benutzername` ASC) ,
-  UNIQUE INDEX `idUser_UNIQUE` (`idUser` ASC) )
+
+  `Name` VARCHAR(20) NOT NULL ,
+
+  `Last_Name` VARCHAR(45) NOT NULL ,
+
+  PRIMARY KEY (`idUser`) )
+
 ENGINE = InnoDB;
 
 
+
+
+
 -- -----------------------------------------------------
--- Table `mydb`.`Kontakdaten`
+
+-- Table `mydb`.`Contact`
+
 -- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `mydb`.`Kontakdaten` (
-  `idKontakdaten` INT NOT NULL AUTO_INCREMENT ,
+
+CREATE  TABLE IF NOT EXISTS `mydb`.`Contact` (
+
+  `idContact` INT NOT NULL AUTO_INCREMENT ,
+
   `UserID` INT NOT NULL ,
+
   `Email` VARCHAR(45) NOT NULL ,
-  `Telefonnummer` VARCHAR(20) NOT NULL ,
-  PRIMARY KEY (`idKontakdaten`) ,
+
+  `Phone_Number` VARCHAR(20) NOT NULL ,
+
+  PRIMARY KEY (`idContact`) ,
+
   UNIQUE INDEX `Email_UNIQUE` (`Email` ASC) )
+
 ENGINE = InnoDB;
+
+
+
+
 
 
 
 SET SQL_MODE=@OLD_SQL_MODE;
+
 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+
 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
+
